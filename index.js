3bbe63fccb23ec18dc6ff1ddbd29c9ea5e459036
Joseph Parker <jparker010190@gmail.com>
2018-07-12 19:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,18 +7,9 @@ function nestedTarget(){
 }
 function increaseRankBy(n){
   const li = document.querySelectorAll('.ranked-list')
-  let result = 1
+  let result = n
   for (let i = 0; i < li.length; i++){
-    li[i].innerHTML = (i+1).toString()
+    li[i].innerHTML = (i+n).toString()
   }
 }
-
-const rankedLists = document.querySelectorAll('.ranked-list')
-const firstList = rankedLists[0]
-const secondList = rankedLists[1]
-
-let children = firstList.children
-let start = 1
-for (let i = 0, l = children.length; i < l; i++) {
-  expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3)
 }
